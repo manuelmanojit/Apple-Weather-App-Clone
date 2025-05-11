9a99b286af5292df6cf5521c4630b47c3838bffd
manuelmanojit <maunamove@gmail.com>
2025-05-11 11:32:30 +0200

    Fix icon, time, and temperature display

---
+++ JS/Slider-card.js
@@ -0,0 +1,167 @@
+import { getCurrentHour, getWeather, isNight } from "./main.js";
+
+function generateHour(nextHour, i) {
+  const time = document.createElement("p");
+  time.classList.add("slider-time");
+  //UPDATE TIME DYNAMICALLY
+  let timeText = document.createTextNode("");
+  //THE CURRENT (FIRST) HOUR IS ALWAYS "NOW"
+  if (i === 0) {
+    timeText.nodeValue = "Now";
+  } else {
+    timeText.nodeValue = nextHour;
+  }
+  time.appendChild(timeText);
+  return time;
+}
+
+function generateIcon(nextHour, conditions, isNight) {
+  const sliderIcon = document.createElement("img");
+  sliderIcon.classList.add("slider-icon");
+
+  let iconSet = false; // track if an icon was matched
+
+  const cond = conditions.trim().toLowerCase();
+
+  sliderIcon.src = "";
+  if (cond === "clear") {
+    sliderIcon.src = "../SVGs/clear-night.svg";
+    sliderIcon.style.width = "12px";
+    iconSet = true;
+  }
+
+  if (cond === "partly cloudy") {
+    if (!isNight) {
+      sliderIcon.src = "../SVGs/partly-sunny.svg";
+      sliderIcon.style.width = "22px";
+    } else if (isNight) {
+      sliderIcon.src = "../SVGs/partly-clear-night.svg";
+    }
+    iconSet = true;
+  }
+
+  if (cond === "sunny") {
+    sliderIcon.src = "../SVGs/sunny.svg";
+    sliderIcon.style.width = "16px";
+    iconSet = true;
+  }
+
+  if (
+    cond === "cloudy" ||
+    cond === "overcast" ||
+    cond === "mist" ||
+    cond === "fog" ||
+    cond === "freezing fog" ||
+    cond === "thundery outbreaks possible"
+  ) {
+    sliderIcon.src = "../SVGs/cloudy.svg";
+    iconSet = true;
+  }
+
+  if (
+    cond === "patchy rain possible" ||
+    cond === "patchy rain nearby" ||
+    cond === "patchy light drizzle" ||
+    cond === "light drizzle" ||
+    cond === "freezing drizzle" ||
+    cond === "heavy freezing drizzle" ||
+    cond === "patchy light rain" ||
+    cond === "light rain" ||
+    cond === "moderate rain at times" ||
+    cond === "moderate rain" ||
+    cond === "heavy rain at times" ||
+    cond === "heavy rain" ||
+    cond === "light freezing rain" ||
+    cond === "moderate or heavy freezing rain" ||
+    cond === "light sleet" ||
+    cond === "moderate or heavy sleet" ||
+    cond === "light rain shower" ||
+    cond === "moderate or heavy rain shower" ||
+    cond === "torrential rain shower" ||
+    cond === "light sleet showers" ||
+    cond === "moderate or heavy sleet showers" ||
+    cond === "patchy sleet possible" ||
+    cond === "patchy freezing drizzle possible" ||
+    cond === "patchy light rain with thunder" ||
+    cond === "moderate or heavy rain with thunder" ||
+    cond === "patchy light snow with thunder" ||
+    cond === "moderate or heavy snow with thunder"
+  ) {
+    sliderIcon.src = "../SVGs/rainy.svg";
+    iconSet = true;
+  }
+
+  if (
+    cond === "patchy snow possible" ||
+    cond === "patchy light snow" ||
+    cond === "light snow" ||
+    cond === "patchy moderate snow" ||
+    cond === "moderate snow" ||
+    cond === "patchy heavy snow" ||
+    cond === "heavy snow" ||
+    cond === "light snow showers" ||
+    cond === "moderate or heavy snow showers" ||
+    cond === "blowing snow" ||
+    cond === "blizzard" ||
+    cond === "ice pellets" ||
+    cond === "light showers of ice pellets" ||
+    cond === "moderate or heavy showers of ice pellets"
+  ) {
+    sliderIcon.src = "../SVGs/snowy.svg";
+    iconSet = true;
+  }
+  if (!iconSet) {
+    console.warn("⚠️ No icon matched for condition:", cond);
+  }
+
+  console.log(sliderIcon.src);
+  return sliderIcon;
+}
+
+function generateTemperature(forecast, nextHour) {
+  const sliderTemperature = document.createElement("p");
+  sliderTemperature.classList.add("slider-temperature");
+  const temperatureText = document.createTextNode(
+    `${Math.round(forecast.hourlyForecast[parseInt(nextHour)].temp_c)}°`
+  );
+  sliderTemperature.appendChild(temperatureText);
+  return sliderTemperature;
+}
+
+async function generateSliderTrio() {
+  const forecast = await getWeather();
+  const currentHour = await getCurrentHour();
+
+  //GET SLIDER CONTAINER
+  const sliderContents = document.querySelector(".slider-contents");
+  //LOOP 24 TIMES TO GENERATE ONE DISPLAY FOR EACH HOUR OF THE DAY
+  for (let i = 0; i < 24; i++) {
+    //CREATE SLIDER TRIO
+    const sliderTrio = document.createElement("div");
+    sliderTrio.classList.add("slider-trio");
+    //MAKE TIME INCREASE BY "i" AT EACH LOOP (first is time + 0)
+    const getNextHour = (currentHour + i) % 24;
+    //ADD A "0" AT THE BEGINNING OF EACH SINGLE NUMBER
+    const nextHour = getNextHour.toString().padStart(2, "0");
+
+    //⏰ GET TIME
+    const sliderTime = generateHour(nextHour, i);
+    //🌥️ DISPLAY ICON
+    const night = await isNight();
+    const conditions =
+      forecast.hourlyForecast[parseInt(nextHour)].condition.text;
+    console.log(conditions);
+    const sliderIcon = generateIcon(nextHour, conditions, night);
+    //🌡️ DISPLAY TEMPERATURE
+    const sliderTemperature = generateTemperature(forecast, nextHour);
+
+    //APPEND CONTENTS TO TRIO
+    sliderTrio.append(sliderTime, sliderIcon, sliderTemperature);
+    //APPEND TRIO CONTAINER TO SLIDER CONTAINER
+    sliderContents.appendChild(sliderTrio);
+
+    console.log(nextHour);
+  }
+}
+
+console.log(generateSliderTrio());
--- JS/main.js
+++ JS/main.js
@@ -1,28 +1,30 @@
-function getCurrentHour() {
-  const currentDay = new Date();
-  const currentHour = currentDay.getHours();
-  return currentHour;
-}
-
+//GENERATE WEATHER OBJECT
 async function getWeather() {
   const API = "138593b09da6432e891161652250905";
-  const city = "Copenhagen";
+  let city = "Copenhagen";
+
+  const search = document.querySelector(".search");
+  search.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      city = search.value;
+    }
+  });
+
   const weatherURL = `https://api.weatherapi.com/v1/forecast.json?key=${API}&q=${city}&days=10&aqi=yes&alerts=no`;
   const astronomyURL = `https://api.weatherapi.com/v1/astronomy.json?key=${API}&q=${city}`;
-
-  //WITH .then() I HANDLE THE PROMISE DIRECTLY INSIDE OF IT, SO I DON'T STORE IT IN A VARIABLE
+  //WEATHER REQUEST
   const weatherResp = await fetch(weatherURL);
   if (!weatherResp.ok) {
     throw new Error("Network response not OK");
   }
   const weather = await weatherResp.json();
-
+  //ASTRONOMY REQUEST
   const moonResp = await fetch(astronomyURL);
   if (!moonResp.ok) {
     throw new Error("Network response not OK");
   }
   const astronomy = await moonResp.json();
-
+  //POPULATING FORECAST OBJECT TO USE FOR THE APP
   const forecast = {
     conditions: weather.current.condition.text,
     temperature: weather.current.temp_c,
@@ -39,44 +41,28 @@ async function getWeather() {
     sunset: astronomy.astronomy.astro.sunset,
     dailyForecast: weather.forecast.forecastday[0],
     hourlyForecast: weather.forecast.forecastday[0].hour,
+    timezone: weather.location.tz_id,
   };
-  console.log(forecast);
-  // console.log(astronomy);
+  console.log(weather);
   return forecast;
 }
 
-function airQuality(air) {
-  const aqi = air;
-  let resp;
-  if (aqi >= 0 && aqi <= 50) return (resp = "Good");
-  if (aqi >= 51 && aqi <= 100) return (resp = "Moderate");
-  if (aqi >= 101 && aqi <= 150)
-    return (resp = "Unhealthy for Sensitive Groups");
-  if (aqi >= 151 && aqi <= 200) return (resp = "Unhealthy");
-  if (aqi >= 201 && aqi <= 300) return (resp = "Very Unhealthy");
-  if (aqi >= 301 && aqi <= 500) return (resp = "Hazardous");
-  resp = "Invalid AQI value";
-  return resp;
+// GET CURRENT HOUR
+async function getCurrentHour() {
+  const currentDay = new Date();
+  const weather = await getWeather();
+  const timezone = weather.timezone;
+  const options = { hour: "numeric", timeZone: timezone, hour12: false };
+  const currentHour = parseInt(
+    new Intl.DateTimeFormat("default", options).format(currentDay)
+  );
+  console.log(currentHour, timezone);
+  return currentHour;
 }
 
-async function getSunsetTime() {
-  const forecast = await getWeather();
-  const time = forecast.sunset.slice(0, 2);
-  //I CAN'T DO slice(6,8) BECAUSE STRING LENGTH MIGHT CHANGE
-  const period = forecast.sunset.slice(-2);
-  //HANDLE MIDNIGHT TIME
-  if (period === "AM" && time === "12") {
-    return "00";
-  }
-  //HANDLE 1PM TO 11PM
-  if (period === "PM" && time !== "12") {
-    let sunset = (parseInt(time) + 12).toString().padStart(2, "0");
-    return sunset;
-  }
-  // FOR OTHER AM TIMES JUST RETURN THE HOUR
-  return time;
-}
+getCurrentHour();
 
+//GET SUNRISE TIME
 async function getSunriseTime() {
   const forecast = await getWeather();
   const time = forecast.sunrise.slice(0, 2);
@@ -95,155 +81,70 @@ async function getSunriseTime() {
   return time;
 }
 
-getSunsetTime(getWeather());
-
-async function generateSliderTrio() {
-  // const conditions = await getWeatherByCity();
+//GET SUNSET TIME
+async function getSunsetTime() {
   const forecast = await getWeather();
-  const sunset = await getSunsetTime();
-  const sunrise = await getSunriseTime();
-
-  //GET SLIDER CONTAINER
-  const sliderContents = document.querySelector(".slider-contents");
-
-  //LOOP 24 TIMES TO GENERATE ONE DISPLAY FOR EACH HOUR OF THE DAY
-  for (let i = 0; i < 24; i++) {
-    //CREATE SLIDER TRIO
-    const sliderTrio = document.createElement("div");
-    //SET ATTRIBUTE TO SLIDER TRIO
-    sliderTrio.classList.add("slider-trio");
-    //DISPLAY TIME
-    const sliderTime = document.createElement("p");
-    sliderTime.classList.add("slider-time");
-    //MAKE TIME INCREASE BY ONE HOUR AT EACH LOOP
-    const getNextHour = (getCurrentHour() + i) % 24;
-    //ADD A "0" AT THE BEGINNING OF EACH SINGLE NUMBER
-    const nextHour = getNextHour.toString().padStart(2, "0");
-    //UPDATE TIME DYNAMICALLY
-    let timeText = document.createTextNode("");
-    //THE CURRENT (FIRST) HOUR IS ALWAYS "NOW"
-    if (i === 0) {
-      timeText.nodeValue = "Now";
-    } else {
-      timeText.nodeValue = nextHour;
-    }
-    sliderTime.appendChild(timeText);
-
-    //DISPLAY ICON
-    const conditions =
-      forecast.hourlyForecast[parseInt(nextHour)].condition.text;
-    console.log(conditions);
-    const sliderIcon = document.createElement("img");
-    sliderIcon.classList.add("slider-icon");
-    if (
-      nextHour > parseInt(sunset) &&
-      nextHour < parseInt(sunrise) &&
-      conditions === "Clear"
-    ) {
-      sliderIcon.src = "../SVGs/clear-night.svg";
-      sliderIcon.style.width = "12px";
-    }
-
-    if (
-      nextHour > parseInt(sunset) &&
-      nextHour < parseInt(sunrise) &&
-      conditions === "Partly cloudy"
-    ) {
-      sliderIcon.src = "../SVGs/partly-clear-night.svg";
-    }
-
-    if (
-      conditions === "Clear" ||
-      (conditions === "Sunny" &&
-        nextHour < parseInt(sunset) &&
-        nextHour > parseInt(sunrise))
-    ) {
-      sliderIcon.src = "../SVGs/sunny.svg";
-      sliderIcon.style.width = "16px";
-    }
-
-    if (conditions === "Partly cloudy") {
-      sliderIcon.src = "../SVGs/partly-sunny.svg";
-      sliderIcon.style.width = "22px";
-    }
-
-    if (
-      conditions === "Cloudy" ||
-      conditions === "Overcast" ||
-      conditions === "Mist" ||
-      conditions === "Fog" ||
-      conditions === "Freezing fog" ||
-      conditions === "Thundery outbreaks possible"
-    ) {
-      sliderIcon.src = "../SVGs/cloudy.svg";
-    }
-
-    if (
-      conditions === "Patchy rain possible" ||
-      conditions === "Patchy light drizzle" ||
-      conditions === "Light drizzle" ||
-      conditions === "Freezing drizzle" ||
-      conditions === "Heavy freezing drizzle" ||
-      conditions === "Patchy light rain" ||
-      conditions === "Light rain" ||
-      conditions === "Moderate rain at times" ||
-      conditions === "Moderate rain" ||
-      conditions === "Heavy rain at times" ||
-      conditions === "Heavy rain" ||
-      conditions === "Light freezing rain" ||
-      conditions === "Moderate or heavy freezing rain" ||
-      conditions === "Light sleet" ||
-      conditions === "Moderate or heavy sleet" ||
-      conditions === "Light rain shower" ||
-      conditions === "Moderate or heavy rain shower" ||
-      conditions === "Torrential rain shower" ||
-      conditions === "Light sleet showers" ||
-      conditions === "Moderate or heavy sleet showers" ||
-      conditions === "Patchy sleet possible" ||
-      conditions === "Patchy freezing drizzle possible" ||
-      conditions === "Patchy light rain with thunder" ||
-      conditions === "Moderate or heavy rain with thunder" ||
-      conditions === "Patchy light snow with thunder" ||
-      conditions === "Moderate or heavy snow with thunder"
-    ) {
-      sliderIcon.src = "../SVGs/rainy.svg";
-    }
-
-    if (
-      conditions === "Patchy snow possible" ||
-      conditions === "Patchy light snow" ||
-      conditions === "Light snow" ||
-      conditions === "Patchy moderate snow" ||
-      conditions === "Moderate snow" ||
-      conditions === "Patchy heavy snow" ||
-      conditions === "Heavy snow" ||
-      conditions === "Light snow showers" ||
-      conditions === "Moderate or heavy snow showers" ||
-      conditions === "Blowing snow" ||
-      conditions === "Blizzard" ||
-      conditions === "Ice pellets" ||
-      conditions === "Light showers of ice pellets" ||
-      conditions === "Moderate or heavy showers of ice pellets"
-    ) {
-      sliderIcon.src = "../SVGs/snowy.svg";
-    }
-
-    //DISPLAY TEMPERATURE
-    const sliderTemperature = document.createElement("p");
-    sliderTemperature.classList.add("slider-temperature");
-    const temperatureText = document.createTextNode(
-      `${Math.round(forecast.hourlyForecast[parseInt(nextHour)].temp_c)}°`
-    );
-    sliderTemperature.appendChild(temperatureText);
-
-    //APPEND CONTENTS TO TRIO
-    sliderTrio.append(sliderTime, sliderIcon, sliderTemperature);
-    //APPEND TRIO CONTAINER TO SLIDER CONTAINER
-    sliderContents.appendChild(sliderTrio);
-    console.log(nextHour);
+  const time = forecast.sunset.slice(0, 2);
+  //I CAN'T DO slice(6,8) BECAUSE STRING LENGTH MIGHT CHANGE
+  const period = forecast.sunset.slice(-2);
+  //HANDLE MIDNIGHT TIME
+  if (period === "AM" && time === "12") {
+    return "00";
   }
+  //HANDLE 1PM TO 11PM
+  if (period === "PM" && time !== "12") {
+    let sunset = (parseInt(time) + 12).toString().padStart(2, "0");
+    return sunset;
+  }
+  // FOR OTHER AM TIMES JUST RETURN THE HOUR
+  return time;
 }
 
-function setSliderIcon() {}
+function airQuality(air) {
+  const aqi = air;
+  let resp;
+  if (aqi >= 0 && aqi <= 50) return (resp = "Good");
+  if (aqi >= 51 && aqi <= 100) return (resp = "Moderate");
+  if (aqi >= 101 && aqi <= 150)
+    return (resp = "Unhealthy for Sensitive Groups");
+  if (aqi >= 151 && aqi <= 200) return (resp = "Unhealthy");
+  if (aqi >= 201 && aqi <= 300) return (resp = "Very Unhealthy");
+  if (aqi >= 301 && aqi <= 500) return (resp = "Hazardous");
+  resp = "Invalid AQI value";
+  return resp;
+}
 
-generateSliderTrio(getWeather());
+async function isNight() {
+  //if the current hour is
+  const now = await getCurrentHour();
+  const sunrise = await getSunriseTime();
+  const sunset = await getSunsetTime();
+
+  let verdict;
+
+  if (sunset !== 0 && sunrise !== 0) {
+    if ((now >= 0 && now < sunrise) || (now > sunset && now <= 23)) {
+      verdict = true;
+    } else {
+      verdict = false;
+    }
+  }
+  if (sunset === 0) {
+    if (now > sunset && now < sunrise) {
+      verdict = true;
+    } else {
+      verdict = false;
+    }
+  }
+  if (sunrise === 0) {
+    if (now > sunset && now <= 23) {
+      verdict = true;
+    } else {
+      verdict = false;
+    }
+  }
+  console.log(now, sunrise, sunset, verdict);
+  return verdict;
+}
+
+export { getCurrentHour, getSunriseTime, getSunsetTime, getWeather, isNight };
